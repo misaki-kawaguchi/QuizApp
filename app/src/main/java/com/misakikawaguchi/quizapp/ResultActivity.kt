b8c102373cacd484f524c8fef381a0a2b34911e9
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 22:32:06 +0900

    [Add]ステータスバーを隠す

--- app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
@@ -2,10 +2,14 @@ package com.misakikawaguchi.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // ステータスバーを隠す
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
