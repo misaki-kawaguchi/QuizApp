9043e2636fbfcd09dabaafe1d16649a86ce20cf3
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 22:35:48 +0900

    [Add]スコアを表示

--- app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
@@ -16,5 +16,12 @@ class ResultActivity : AppCompatActivity() {
         // USER_NAMEから名前を取得し変数に代入、テキスtを表示
         val userName = intent.getStringExtra(Constants.USER_NAME)
         tv_name.text = userName
+
+        // TOTAL_QUESTIONSからトータルの質問数を、CORRECT_ANSWERSから正解数を取得し変数に代入、デフォルトは0
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        // スコアを表示
+        tv_score.text = "Your Score is $correctAnswers out of $totalQuestions."
     }
 }
