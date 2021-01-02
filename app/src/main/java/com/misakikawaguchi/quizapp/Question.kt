3861737fa3fb761e515f6674b55f68ba343b67ac
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 17:14:49 +0900

    [Add]質問データモデルクラスを作成

---
+++ app/src/main/java/com/misakikawaguchi/quizapp/Question.kt
@@ -0,0 +1,13 @@
+package com.misakikawaguchi.quizapp
+
+// 質問データモデルクラスを作成
+data class Question (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
