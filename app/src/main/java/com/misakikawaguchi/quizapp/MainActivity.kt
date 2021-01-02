5188a27b66ffc5be6930a0364bf7be342ea0cb2d
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 17:09:24 +0900

    [Add]QuizQuestionsActivityを起動する処理追加

--- app/src/main/java/com/misakikawaguchi/quizapp/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.misakikawaguchi.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,17 @@ class MainActivity : AppCompatActivity() {
 
         // ステータスバーを隠してフルスクリーン表示にする
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // 名前が入力されているかどうかを検証し、入力されている場合はQuizQuestion画面を起動する
+        // 入力されていない場合はトーストを表示する
+        btn_start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
