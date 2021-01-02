b4745fc2ff31154d85fed968e3c4da07b5a3fc17
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 19:21:52 +0900

    [Add]UIに質問を表示できるように設定する

--- app/src/main/java/com/misakikawaguchi/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.misakikawaguchi.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,24 @@ class QuizQuestionsActivity : AppCompatActivity() {
         for (i in questionsList) {
             Log.e("Questions", i.question)
         }
+
+        // リストからUIに質問を設定する
+        // 今何問目から表示。最初なので1を設定する
+        val currentPosition = 1
+        // 問題を表示する。0からスタートなので、currentPosition - 1と設定
+        val question: Question? = questionsList[currentPosition - 1]
+
+        // progressBarの進捗状況を設定する
+        progressBar.progress = currentPosition
+        // progressBarのテキストを設定
+        tv_progress.text = "$currentPosition" + "/" + progressBar.getMax()
+
+        // 現在の質問とオプションを設定する
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
     }
 }
