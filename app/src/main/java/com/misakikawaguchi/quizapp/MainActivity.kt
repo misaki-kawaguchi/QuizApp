23b7fd61b659f463dc4e6e4c1819f83b867b0002
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 16:28:15 +0900

    [Add]ステータスバーを隠してフルスクリーン表示にする

--- app/src/main/java/com/misakikawaguchi/quizapp/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.misakikawaguchi.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ステータスバーを隠してフルスクリーン表示にする
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
