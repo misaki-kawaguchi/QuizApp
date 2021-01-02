9841e5b827a975d379ce0d8f132e3754989c2015
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 22:34:08 +0900

    [Add]ユーザーネームを表示する

--- app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/ResultActivity.kt
@@ -3,6 +3,7 @@ package com.misakikawaguchi.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,9 @@ class ResultActivity : AppCompatActivity() {
 
         // ステータスバーを隠す
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // USER_NAMEから名前を取得し変数に代入、テキスtを表示
+        val userName = intent.getStringExtra(Constants.USER_NAME)
+        tv_name.text = userName
     }
 }
