9edbe08d45ecd23d5c0619f23611f30ae95451ed
川口美咲 <misaki@arcphilia.co.jp>
2021-01-02 18:10:50 +0900

    [Add]質問のリストをUIに表示できるようにする

--- app/src/main/java/com/misakikawaguchi/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/misakikawaguchi/quizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,18 @@ package com.misakikawaguchi.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        // 質問のリストを取得して、UIに表示する。ログに出力する
+        val questionsList = Constants.getQuestions()
+        Log.e("Questions Size", "${questionsList.size}")
+        for (i in questionsList) {
+            Log.e("Questions", i.question)
+        }
     }
 }
